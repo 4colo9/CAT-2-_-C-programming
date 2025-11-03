5e36e55a4797c8ea4ef3d40866d09af25e0b2184
4colo9 <shawnwakhu@gmail.com>
2025-11-04 00:15:26 +0300

    Refactor net pay calculation program

--- CAT 2 .Q 2.C
+++ CAT 2 .Q 2.C
@@ -4,34 +4,34 @@ PA106/G/28764/25
 
 A programme that calculates the net pay*/
 
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main() {
+    float hoursWorked, hourlyWage, grossPay, taxes, netPay;
 
-float hoursWorked,hourlyWage,grossPay,Taxes,NetPay,NormalPay;
+    printf("Enter hours worked this week: ");
+    scanf("%f", &hoursWorked);
 
-printf("What are the hours you worked this week :\n");
-scanf("%f",&hoursWorked);
+    printf("Enter your hourly wage: ");
+    scanf("%f", &hourlyWage);
 
-printf("What is your hourly wage :");
-scanf("%f",&hourlyWage);
+    if (hoursWorked > 40) {
+        grossPay = (40 * hourlyWage) + (hoursWorked - 40) * (1.5 * hourlyWage);
+    } else {
+        grossPay = hoursWorked * hourlyWage;
+    }
 
-NormalPay=hourlyWage*hoursWorked;
+    if (grossPay <= 600) {
+        taxes = 0.15 * grossPay;
+    } else {
+        taxes = (0.15 * 600) + (0.20 * (grossPay - 600));
+    }
 
-if(hoursWorked > 40){
-    grossPay=1.5*NormalPay;}
-else{grossPay = NormalPay;}
+    netPay = grossPay - taxes;
 
+    printf("\nYour gross pay is: $%.2f\n", grossPay);
+    printf("Your taxes are: $%.2f\n", taxes);
+    printf("Your net pay is: $%.2f\n", netPay);
 
-if(grossPay < 600 ){Taxes = 0.15 * grossPay;}
-else{Taxes = (0.15 * 600 )+ 0.2*(grossPay-600) ;}
-
-NetPay=grossPay-Taxes;
-
-printf("Your gross pay is %0.2f",grossPay);
-printf("Your taxes are %0.2f",Taxes);
-printf("Your net pay is %0.2f",NetPay);
-
-return 0;
+    return 0;
 }
-
